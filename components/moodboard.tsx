08f4c8beeaece3effc11692cc33940aec7518f8d
Lee Jaehyu <goodbright0@gmail.com>
2023-06-27 18:01:07 +0900

    g

--- components/moodboard.tsx
+++ components/moodboard.tsx
@@ -1,6 +1,6 @@
 'use client';
-import Image from 'next/image';
 
+import Image from 'next/image';
 import { useEffect } from 'react';
 
 import { Gradient } from 'public/MeshGradient.js';
@@ -16,10 +16,11 @@ export function LVMHMoodBoard({}: {}) {
   };
 
   useEffect(() => {
-    const gradient = new Gradient();
+    const gradient1 = new Gradient();
+    const gradient2 = new Gradient();
     // @ts-ignore
-    gradient.initGradient('#gradient-canvas');
-
+    gradient1.initGradient('#gradient1-canvas');
+    gradient2.initGradient('#gradient2-canvas');
     //backgroundAnimation();
   }, []);
 
@@ -29,7 +30,7 @@ export function LVMHMoodBoard({}: {}) {
         id="sectionintregue"
         className=" relative top-0 flex  h-screen  w-full items-center justify-center "
       >
-        <canvas className="gradientStyles" id="gradient-canvas" data-transition-in />
+        <canvas className="gradientStyles" id="gradient1-canvas" data-transition-in />
 
         <div className="h-screen w-full">
           <>
@@ -39,7 +40,7 @@ export function LVMHMoodBoard({}: {}) {
                 autoPlay={true}
                 muted={true}
                 loop={true}
-                className="hidden h-[33vh] w-[33%] animate-videoFadeIn sm:block"
+                className="hidden  animate-videoFadeIn sm:block"
               >
                 <source src="LVMH-Hero-vorbis.webm" type="video/webm" />
               </video>
@@ -49,62 +50,52 @@ export function LVMHMoodBoard({}: {}) {
       </div>
 
       <section className="lv-landing-page__section -moodboard_01 -padding-top-3 -padding-top-m-5  relative top-0 flex h-screen w-full items-center justify-center">
-        <div id="moodboard_01" className="lv-editorial-moodboard -layout-aside -reverse flex">
-          <div className="lv-editorial-moodboard__medias w-[50vw]">
-            <div className="lv-editorial-moodboard__main">
-              <div className="lv-editorial-media">
-                <div className="lv-featured-products lv-editorial-featured-products placeholder-portrait">
-                  <div aria-label="View featured products" className="lv-featured-products__wrap">
-                    <div className="lv-smart-picture lv-responsive-picture">
-                      <Image
-                        src="/lvmh-moodboard-left.avif"
-                        alt="interior-1"
-                        fill={true}
-                        sizes="1200"
-                        className="relative"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <canvas className="gradientStyles" id="gradient2-canvas" data-transition-in />
+        <div
+          id="moodboard_01"
+          className="lv-editorial-moodboard -layout-aside -reverse flex flex-initial"
+        >
+          <div id="media" className="inset-0 w-[56.6vw]">
+            <Image
+              src="/cms_vis_txt_img_desktop_5049_v_1.jpg"
+              alt="interior-1"
+              width="900"
+              height="1200"
+              sizes="60vw"
+              className="relative"
+              style={{ objectFit: 'contain' }}
+            />
           </div>
 
-          <div className="lv-editorial-moodboard__content relative flex-1">
-            <div className="flex flex-col">
-              <div className=" lv-editorial-moodboard__details h-[300px] w-[50vw]">
-                <div className="lv-editorial-text-block lv-editorial-moodboard__description">
-                  <div className="lv-editorial-text-block__description body-l">
-                    <p>
-                      An enduring feminine icon, the Capucines symbolizes the quintessence of Louis
-                      Vuitton, crafted with An intuitive blend of elegance, versatility, and
-                      savoir-faire. Reimagined in a vibrant spring hue, Zendaya showcases the iconic
-                      handbag in Topaz blue.
-                    </p>
-                  </div>
-                </div>
+          <div id="description" className=" flex w-[40vw] flex-col">
+            <div className="lv-editorial-text-block__description body-l flex  h-[33vh] justify-end  ">
+              <div
+                className=" mr-2 h-[196px] w-[313px] translate-y-9 transform leading-7"
+                style={{
+                  transform: 'translateY(30px)',
+                  opacity: 0.25
+                }}
+              >
+                <p>
+                  An enduring feminine icon, the Capucines symbolizes the quintessence of Louis
+                  Vuitton, crafted with An intuitive blend of elegance, versatility, and
+                  savoir-faire. Reimagined in a vibrant spring hue, Zendaya showcases the iconic
+                  handbag in Topaz blue.
+                </p>
               </div>
-              <div className="lv-editorial-moodboard__medias">
-                <div className="lv-editorial-moodboard__main">
-                  <div className="lv-editorial-media">
-                    <div className="lv-featured-products lv-editorial-featured-products placeholder-portrait">
-                      <div
-                        aria-label="View featured products"
-                        className="lv-featured-products__wrap"
-                      >
-                        <div className="lv-smart-picture lv-responsive-picture">
-                          <Image
-                            src="/CAPUCINES_ZENDAYA_ANNOUNCEMENT_SP2_LVCOM_1600x2000_DII.avif"
-                            alt="interior-1"
-                            fill={true}
-                            sizes="1200"
-                            className="relative"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+            </div>
+            <div className="lv-editorial-text-block__description body-l h-[67vh]">
+              <div id="Hero" className="mt-12 flex h-full w-full items-center justify-center">
+                <video
+                  controls={false}
+                  autoPlay={true}
+                  muted={true}
+                  loop={true}
+                  className="hidden h-full w-full animate-videoFadeIn sm:block"
+                  style={{ objectFit: 'contain' }}
+                >
+                  <source src="xzFaPUbqp2_MD.mp4" type="video/webm" />
+                </video>
               </div>
             </div>
           </div>
